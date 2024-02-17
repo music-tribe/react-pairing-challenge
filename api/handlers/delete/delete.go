76e1127a9837f31ba2fb830d798891e553608d9b
John Hughes <john.hughes@musictribe.com>
2024-02-17 12:39:24 +0000

    feat: :sparkles: implement Delete handler

---
+++ api/handlers/delete/delete.go
@@ -0,0 +1,47 @@
+package delete
+
+import (
+	"net/http"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/labstack/echo/v4"
+	"github.com/music-tribe/react-pairing-challenge/database"
+	"github.com/music-tribe/uuid"
+)
+
+//go:generate mockgen -destination=./mocks/delete.go -package=deletemocks -source=delete.go
+type DeleteDatabase interface {
+	Delete(id uuid.UUID) error
+}
+
+type DeleteRequest struct {
+	TaskId uuid.UUID `param:"taskId" validate:"required"`
+}
+
+func Delete(db DeleteDatabase) func(echo.Context) error {
+	if db == nil {
+		panic("delete.Delete: db has nil value")
+	}
+
+	return func(c echo.Context) error {
+		req := DeleteRequest{}
+
+		if err := c.Bind(&req); err != nil {
+			return echo.NewHTTPError(http.StatusBadRequest, err)
+		}
+
+		if err := validator.New().Struct(&req); err != nil {
+			return echo.NewHTTPError(http.StatusBadRequest, err)
+		}
+
+		err := db.Delete(req.TaskId)
+		if err != nil {
+			if err == database.ErrNotFound {
+				return echo.NewHTTPError(http.StatusNotFound, err)
+			}
+			return echo.NewHTTPError(http.StatusInternalServerError, err)
+		}
+
+		return c.String(http.StatusOK, "DELETED")
+	}
+}
---
+++ api/handlers/delete/delete_test.go
@@ -0,0 +1,135 @@
+package delete
+
+import (
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/labstack/echo/v4"
+	"github.com/music-tribe/react-pairing-challenge/database"
+	deletemocks "github.com/music-tribe/react-pairing-challenge/handlers/delete/mocks"
+	"github.com/music-tribe/uuid"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDelete(t *testing.T) {
+	e := echo.New()
+
+	t.Run("when the db has a nil value, we should panic", func(t *testing.T) {
+		assert.Panics(t, func() {
+			Delete(nil)
+		})
+	})
+
+	t.Run("when the id is missing we should return a 400 error", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+		db := deletemocks.NewMockDeleteDatabase(ctrl)
+
+		req := httptest.NewRequest(http.MethodDelete, "/", nil)
+		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		rec := httptest.NewRecorder()
+		ctx := e.NewContext(req, rec)
+		ctx.SetParamNames("taskId")
+		ctx.SetParamValues("")
+
+		err := Delete(db)(ctx)
+		assert.ErrorContains(t, err, "invalid UUID length: 0")
+		assert.Equal(t, http.StatusBadRequest, deleteStatusCode(rec, err))
+	})
+
+	t.Run("when the id has a nil value we should return a 400 error", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+		db := deletemocks.NewMockDeleteDatabase(ctrl)
+
+		req := httptest.NewRequest(http.MethodDelete, "/", nil)
+		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		rec := httptest.NewRecorder()
+		ctx := e.NewContext(req, rec)
+		ctx.SetParamNames("taskId")
+		ctx.SetParamValues(uuid.Nil.String())
+
+		err := Delete(db)(ctx)
+		assert.ErrorContains(t, err, "Error:Field validation for 'TaskId' failed on the 'required' tag")
+		assert.Equal(t, http.StatusBadRequest, deleteStatusCode(rec, err))
+	})
+
+	t.Run("when we the record can't be found we should return a 404 error", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+		db := deletemocks.NewMockDeleteDatabase(ctrl)
+
+		id := uuid.New()
+		req := httptest.NewRequest(http.MethodDelete, "/", nil)
+		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		rec := httptest.NewRecorder()
+		ctx := e.NewContext(req, rec)
+		ctx.SetParamNames("taskId")
+		ctx.SetParamValues(id.String())
+
+		db.EXPECT().Delete(id).Return(database.ErrNotFound)
+
+		err := Delete(db)(ctx)
+		assert.ErrorContains(t, err, database.ErrNotFound.Error())
+		assert.Equal(t, http.StatusNotFound, deleteStatusCode(rec, err))
+	})
+
+	t.Run("when we delete an unknown error from the db we should return a 500 error", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+		db := deletemocks.NewMockDeleteDatabase(ctrl)
+
+		id := uuid.New()
+		req := httptest.NewRequest(http.MethodDelete, "/", nil)
+		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		rec := httptest.NewRecorder()
+		ctx := e.NewContext(req, rec)
+		ctx.SetParamNames("taskId")
+		ctx.SetParamValues(id.String())
+
+		db.EXPECT().Delete(id).Return(errors.New("some error"))
+
+		err := Delete(db)(ctx)
+		assert.ErrorContains(t, err, "some error")
+		assert.Equal(t, http.StatusInternalServerError, deleteStatusCode(rec, err))
+	})
+
+	t.Run("when the request is well formed, we should delete the task back and no error", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+		db := deletemocks.NewMockDeleteDatabase(ctrl)
+
+		id := uuid.New()
+		req := httptest.NewRequest(http.MethodDelete, "/", nil)
+		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		rec := httptest.NewRecorder()
+		ctx := e.NewContext(req, rec)
+		ctx.SetParamNames("taskId")
+		ctx.SetParamValues(id.String())
+
+		db.EXPECT().Delete(id).Return(nil)
+
+		err := Delete(db)(ctx)
+		assert.NoError(t, err)
+		assert.Equal(t, http.StatusOK, deleteStatusCode(rec, err))
+
+		assert.Equal(t, "DELETED", rec.Body.String())
+		assert.NoError(t, err)
+	})
+}
+
+func deleteStatusCode(rec *httptest.ResponseRecorder, err error) int {
+	if err == nil {
+		return rec.Code
+	}
+
+	hterr := &echo.HTTPError{}
+	if errors.As(err, &hterr) {
+		return hterr.Code
+	}
+
+	return 500
+}
